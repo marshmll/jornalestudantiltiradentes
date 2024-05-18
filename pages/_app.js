3079866bb121ec0974ebff68b57fa5ec27f0bfc7
mashmll <renandasilvaoliveiraandrade@gmail.com>
2024-05-18 10:14:00 -0300

    Add meta tags for SEO

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,47 @@ export default function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+
+        {/* Primary Meta Tags */}
         <title>Jornal Estudantil Tiradentes</title>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Jornal Estudantil Tiradentes" />
+        <meta
+          name="description"
+          content="O Jornal do Estudante Bem-informado. Tenha acesso à informações produzidas pela comunidade escolar do Colégio Estadual Tiradentes."
+        />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://jornalestudantiltiradentes.vercel.app"
+        />
+        <meta property="og:title" content="Jornal Estudantil Tiradentes" />
+        <meta
+          property="og:description"
+          content="O Jornal do Estudante Bem-informado. Tenha acesso à informações produzidas pela comunidade escolar do Colégio Estadual Tiradentes."
+        />
+        <meta
+          property="og:image"
+          content="https://www.datocms-assets.com/119746/1716037665-captura-de-tela-de-2024-05-15-20-16-23.png"
+        />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://jornalestudantiltiradentes.vercel.app"
+        />
+        <meta property="twitter:title" content="Jornal Estudantil Tiradentes" />
+        <meta
+          property="twitter:description"
+          content="O Jornal do Estudante Bem-informado. Tenha acesso à informações produzidas pela comunidade escolar do Colégio Estadual Tiradentes."
+        />
+        <meta
+          property="twitter:image"
+          content="https://www.datocms-assets.com/119746/1716037665-captura-de-tela-de-2024-05-15-20-16-23.png"
+        />
       </Head>
       <Layout>
         <Component {...pageProps} />
